8ffeabd516b5d5a2c2add4e31f87e24f0d8fe26f
Shereef Bishay <shereef@gmail.com>
2012-03-12 14:58:22 -0700

    errors for link form

--- spec/views/links/new.html.erb_spec.rb
+++ spec/views/links/new.html.erb_spec.rb
@@ -1,15 +1,29 @@
 require 'spec_helper'
 
 describe 'links/new' do
-  before do
-    assign(:link, Link.new)
-    render
-  end  
+  context "form" do
+    before do
+      assign(:link, Link.new)
+      render
+    end  
 
-  it 'has a form posting to /links' do
-    rendered.should have_tag('form', :with => { :action => "/links", :method => 'post' }) do
-      with_tag "input", :with => { :name => "link[url]"}
-      with_tag "input", :with => { :name => "link[title]"}
+    it 'has a form posting to /links' do
+      rendered.should have_tag('form', :with => { :action => "/links", :method => 'post' }) do
+        with_tag "input", :with => { :name => "link[url]"}
+        with_tag "input", :with => { :name => "link[title]"}
+      end
+    end
+  end
+  
+  context "errors" do
+    before do
+      link = Link.new
+      link.valid?
+      assign(:link, link)
+      render
+    end
+    it "displays errors when link saving fails" do
+      rendered.should have_content("can't be blank")
     end
   end
 end
